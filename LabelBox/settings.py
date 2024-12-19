3cbd8eec4dad1568baed6c88696ac9b24b7e76dc
PajosTM <josephadeoye79@gmail.com>
2024-12-19 12:33:09 +0100

    Commit

--- LabelBox/settings.py
+++ LabelBox/settings.py
@@ -20,12 +20,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_qbfy6&b-_=o=6ni7jmyh4@oquwnz!5jic1d^oeid46gq!fh45'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*']
+
 
 
 # Application definition
@@ -76,14 +79,15 @@ WSGI_APPLICATION = 'LabelBox.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',  # MySQL Engine
-        'NAME': 'labelbox_db',                # Your database name
-        'USER': 'root',                       # Your MySQL username
-        'PASSWORD': 'Edu-helplimited001',           # Your MySQL password
-        'HOST': 'localhost',                  # Or the IP of your MySQL server
-        'PORT': '3306',                       # Default MySQL port
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'your-db-name',
+        'USER': 'your-db-user',
+        'PASSWORD': 'your-db-password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 
@@ -122,13 +126,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 STATIC_URL = '/static/'
 
+
 # Directory for additional static files
 STATICFILES_DIRS = [
     BASE_DIR / "static",  # Adjust the path to your project's static folder
 ]
 
 # Directory to collect static files for production
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
